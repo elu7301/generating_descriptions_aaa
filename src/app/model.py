7f588a8e90e6c9f25961d375434a0d4cb2fb7593
ILUNOSHA~ <dimakorolev2000@gmail.com>
2024-06-09 23:11:04 +0300

    model to cuda

--- src/app/model.py
+++ src/app/model.py
@@ -16,11 +16,11 @@ lora_config = LoraConfig(
 
 model = get_peft_model(vit, lora_config)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
 vit.load_state_dict(torch.load(r'app/static/state_dict'))
 
 vit.decoder.config.pad_token_id = 0
 vit.decoder.config.bos_token_id = 1
 vit.decoder.config.eos_token_id = 2
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
