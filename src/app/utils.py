1b4a21d921cb284378985956da3b03023d1f9194
ILUNOSHA~ <dimakorolev2000@gmail.com>
2024-06-10 20:20:47 +0300

    to cpu

--- src/app/utils.py
+++ src/app/utils.py
@@ -1,5 +1,5 @@
 from PIL.Image import Image as PilImage
-from .model import feature_extractor, tokenizer_gpt2, model
+from .model import feature_extractor, tokenizer_gpt2, model, device
 
 
 user_data = {}
@@ -18,12 +18,12 @@ def generate_description_logic(input_text: str, image: PilImage) -> str:
         str: Сгенерированное описание объявления.
     """
 
-    pix = feature_extractor(image, return_tensors="pt")
-    text = tokenizer_gpt2(input_text, return_tensors="pt")
+    pix = feature_extractor(image, return_tensors="pt").to(device)
+    text = tokenizer_gpt2(input_text, return_tensors="pt").to(device)
 
     generated_text = model.generate(
-        pixel_values=pix.pixel_values.cuda(),
-        decoder_input_ids=text.input_ids.cuda(),
+        pixel_values=pix.pixel_values,
+        decoder_input_ids=text.input_ids,
         max_length=500,
         eos_token_id=tokenizer_gpt2.eos_token_id,
         do_sample=True,
