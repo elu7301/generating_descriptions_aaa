1ac825d7cf20b1917ceb7cce7f7789f3d3005ae7
ILUNOSHA~ <dimakorolev2000@gmail.com>
2024-06-19 14:11:41 +0300

    final

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,9 +1,6 @@
-from flask import (Blueprint, render_template, request,
-                   redirect, url_for, flash, jsonify, current_app)
+from flask import (Blueprint, render_template, request, jsonify)
 from werkzeug import Response
-from werkzeug.utils import secure_filename
-import os
-from app.utils import generate_description_logic, user_data
+from app.utils import generate_description_logic, promt_preprocess
 from PIL import Image
 import io
 
@@ -12,104 +9,30 @@ main_bp = Blueprint('main', __name__)
 
 
 @main_bp.route('/')
-def index() -> str:
-    """Отображает главную страницу."""
+def category_select() -> str:
+    """
+    Отображает главную страницу.
+    """
 
     return render_template('category_select.html')
 
 
-@main_bp.route('/select_category', methods=['POST'])
-def select_category() -> Response | str:
-    """Обрабатывает выбор категории."""
-
-    category = request.form['category']
-    subcategory = request.form.get('subcategory', '')
-
-    user_data['Вид одежды'] = category
-    user_data['Предмет одежды'] = subcategory
-
-    if category == "Аксессуары":
-        return redirect(url_for('main.ad_details',
-                                category=category, subcategory=''))
-
-    return render_template('ad_details.html',
-                           category=category, subcategory=subcategory)
-
-
-@main_bp.route('/ad_details', methods=['GET'])
+@main_bp.route('/ad_details')
 def ad_details() -> str:
-    """Отображает детали объявления."""
+    """
+    Отображает страницу с заполнением данных.
+    """
 
-    category = request.args.get('category')
-    subcategory = request.args.get('subcategory')
-
-    return render_template('ad_details.html',
-                           category=category, subcategory=subcategory)
-
-
-@main_bp.route('/save_data', methods=['POST'])
-def save_data() -> Response:
-    """Сохраняет данные."""
-
-    field_name = request.form['field_name']
-    field_value = request.form['field_value']
-    user_data[field_name] = field_value
-    return jsonify({'status': 'success'})
-
-
-@main_bp.route('/create_ad', methods=['POST'])
-def create_ad() -> Response:
-    """Создает объявление."""
-
-    title = request.form['title']
-    ad_type = request.form['ad_type']
-    condition = request.form['condition']
-    color = request.form['color']
-    brand = request.form['brand']
-    size = request.form['size']
-
-    image = request.files['image']
-    location = request.form['location']
-
-    user_data['Заголовок'], user_data['Вид обьявления'] = title, ad_type
-    user_data['Состояние'], user_data['Место сделки'] = condition, location
-    user_data['Цвет'], user_data['Бренд'] = color, brand
-    user_data['Размер'] = size
-
-    if image and image.filename != '':
-        filename = secure_filename(image.filename)
-        image_path = os.path.join(current_app.config['UPLOAD_FOLDER'],
-                                  filename)
-        image.save(image_path)
-        flash('Объявление успешно создано!', 'success')
-    else:
-        flash('Ошибка при загрузке изображения', 'error')
-
-    return redirect(url_for('main.index'))
+    return render_template('ad_details.html')
 
 
 @main_bp.route('/generate_description', methods=['POST'])
 def generate_description() -> Response:
     """Генерирует описание."""
-
-    input_text = (f"Заголовок: {user_data['Заголовок']},"
-                  f" Вид одежды: {user_data['Вид одежды']},"
-                  f" Вид обьявления: {user_data['Вид обьявления']},"
-                  f" Место сделки: {user_data['Место сделки']},"
-                  f" Состояние: {user_data['Состояние']},"
-                  f" Цвет: {user_data['Цвет']},"
-                  f" Бренд одежды: {user_data['Бренд']}"
-                  f" Размер. {user_data['Вид одежды']}: {user_data['Размер']}")
-
-    if user_data['Вид одежды'] != 'Аксессуары':
-        input_text += f", Предмет одежды: {user_data['Предмет одежды']}"
-
-    input_text += ', Описание:'
-
+    input_text = promt_preprocess(request.form)
     file = request.files['image']
     image = io.BytesIO(file.read())
     pil_image = Image.open(image).convert('RGB')
 
     description = generate_description_logic(input_text, pil_image)
-
     return jsonify({'description': description})
--- src/app/utils.py
+++ src/app/utils.py
@@ -1,11 +1,34 @@
 from PIL.Image import Image as PilImage
-from .model import feature_extractor, tokenizer_gpt2, model, device
+from .model import feature_extractor, tokenizer_gpt2, model
+from app.constants import promt_dict
+
+def promt_preprocess(user_data: dict) -> str:
+    """
+    Препроцессинг данных объявления для подачи в качестве промта в модель.
+    """
+    input_text = (f"{promt_dict['title']}: {user_data.get('title', '')},"
+                  f" {promt_dict['cat']}: {user_data.get('cat', '')},"
+                  f" {promt_dict['ad_type']}: {user_data.get('ad_type', '')},"
+                  f" {promt_dict['location']}: {user_data.get('location', '')},"
+                  f" {promt_dict['condition']}: {user_data.get('condition', '')},"
+                  f" {promt_dict['color']}: {user_data.get('color', '')},"
+                  f" {promt_dict['brand']}: {user_data.get('brand', '')}"
+                  f" {promt_dict['size']}. {user_data.get('cat', '')}: {user_data.get('size', '')}")
+
+    if user_data.get('cat', '') != promt_dict['accessories']:
+        input_text += f", {promt_dict['sub']}: {user_data.get('sub', '')}"
+
+    input_text += f", {promt_dict['description']}:"
+
+    return input_text
 
 
-user_data = {}
-
-
-def generate_description_logic(input_text: str, image: PilImage) -> str:
+def generate_description_logic(
+        input_text: str,
+        image: PilImage,
+        temperature: float=0.7,
+        max_length: int=500
+) -> str:
     """
     Генерирует описание для объявления.
 
@@ -18,19 +41,19 @@ def generate_description_logic(input_text: str, image: PilImage) -> str:
         str: Сгенерированное описание объявления.
     """
 
-    pix = feature_extractor(image, return_tensors="pt").to(device)
-    text = tokenizer_gpt2(input_text, return_tensors="pt").to(device)
+    pix = feature_extractor(image, return_tensors="pt")
+    text = tokenizer_gpt2(input_text, return_tensors="pt")
 
     generated_text = model.generate(
-        pixel_values=pix.pixel_values,
-        decoder_input_ids=text.input_ids,
-        max_length=500,
+        pixel_values=pix.pixel_values.cuda(),
+        decoder_input_ids=text.input_ids.cuda(),
+        max_length=max_length,
         eos_token_id=tokenizer_gpt2.eos_token_id,
         do_sample=True,
-        temperature=0.7
+        temperature=temperature
     )
 
     description = tokenizer_gpt2.decode(generated_text[0])
-    description = description.split('Описание: ', maxsplit=1)[1].strip()
+    description = description.split(f"{promt_dict['description']}: ", maxsplit=1)[1].strip()
 
     return description.strip('</s>')
